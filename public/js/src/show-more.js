44e5f57a6b1394560e787c492c68b27bcbef5290
nazar yavnyy <ny1am.lviv.ua@gmail.com>
2016-10-28 23:26:20 +0300

    revised show0more component to be ready button specific

--- public/js/src/show-more.js
+++ public/js/src/show-more.js
@@ -1,4 +1,3 @@
-var modernizr = require('modernizr');
 var $ = require('jquery');
 
 (function() {
@@ -21,26 +20,16 @@ var $ = require('jquery');
 		var itemsToShow = parseInt($this.attr('data-show-more-value'));
 		var $element = $('#'+elementId);
 		if ($this.is('[open]')) {
-			$element.removeClass('js-sm-holder');
 			$element.children().removeClass('hidden');
-			$element.children().removeClass('js-sm-shown-on-top');
 		} else {
-			$element.addClass('js-sm-holder');
 			$element.children().removeClass('hidden');
 			var shownCount = 0;
-			if (modernizr.flexbox) {
-				$element.children().has('input:checked').each(function() {
-					if (shownCount < itemsToShow) {
-						$(this).addClass('js-sm-shown-on-top');
-						shownCount++;
-					}
-				});
-			}
-			$element.children().not('.js-sm-shown-on-top').each(function() {
-				if (shownCount < itemsToShow) {
+			$element.children().each(function() {
+				var $el = $(this);
+				if (shownCount < itemsToShow || $el.has('input:checked').length > 0) {
 					shownCount++;
 				} else {
-					$(this).addClass('hidden');
+					$el.addClass('hidden');
 				}
 			});
 		}
