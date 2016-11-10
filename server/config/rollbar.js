719bb092852b5ff9aa6bce825d64573b65e10f9b
nazar yavnyy <ny1am.lviv.ua@gmail.com>
2016-11-10 20:47:17 +0200

    updated dev settings to show stack trace on error

--- server/config/rollbar.js
+++ server/config/rollbar.js
@@ -6,8 +6,12 @@ module.exports = function(app) {
 		app.use(rollbar.errorHandler(VARS.rollbar));
 	}
 	app.use(function(error, request, response, next) {
-		console.log(error.stack);
-		response.status(404);
-		return response.render('misc/404', {layout: 'main--fullpage'});
+		if (process.env.NODE_ENV === 'production') {
+			response.status(404);
+			return response.render('misc/404', {layout: 'main--fullpage'});
+		} else {
+			console.log(error.stack);
+			return response.send(error.stack);
+		}
 	});
 };
