ae04ca1e6351693735bf6d74817bdd7d98e26895
ny1am <ny1am.lviv.ua@gmail.com>
2017-04-04 13:38:27 +0300

    revised radio component

--- public/js/src/browserify-react.js
+++ public/js/src/browserify-react.js
@@ -5,7 +5,6 @@ require('../thirdparty/jquery.serialize-object.js');
 require('./js-enabled.js');
 require('./href.js');
 
-require('./radio.js');
 require('./multiple-select.js');
 require('./password.js');
 require('./show-more.js');
--- server/react-views/components/Radio.jsx
+++ server/react-views/components/Radio.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
 class Radio extends React.Component {
-	renderRadio() {
-		return React.createElement(
-			'input',
-			Object.assign({type: 'radio'}, this.props)
-		)
+
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {checked: props && props.defaultChecked};
 	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.checked !== this.props.checked) {
+			this.setState({checked: nextProps.checked});
+		}
+	}
+
+	handleChange(el) {
+		if (this.props && this.props.onChange) {
+			this.props.onChange(el);
+		}
+		this.setState({checked: el.target.checked});
+	}
+
 	render() {
 		return (
-			<div className="radio">
-				{this.renderRadio()}
+			<div className="radio" {...this.state.checked?{'data-checked':''}:{}}>
+				<input type="radio" {...this.props} onChange={this.handleChange}/>
 				<label htmlFor={this.props.id}></label>
 			</div>
 		);
