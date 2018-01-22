969295c7be6f7f0cbb0b95ae1bfcc9df94a945f0
ny1am <ny1am.lviv.ua@gmail.com>
2018-01-22 17:06:02 +0200

    refa: changed company endpoint result signature

--- server/controllers/company.js
+++ server/controllers/company.js
@@ -1,4 +1,5 @@
 var api = require('../api/company.js');
+var _ = require('lodash');
 
 
 exports.get = function(request, response, next) {
@@ -8,7 +9,20 @@ exports.get = function(request, response, next) {
     if (err) {
       return next(err);
     } else {
-      return response.send(model);
+      //todo: change in database or whatever
+      var newModel = {
+        company: {
+          _id: model.company._id,
+          img: model.company.img,
+          title: model.company.title,
+          company_site: model.company.company_site,
+          description: model.company.description,
+          loyalty: model.company.loyalty,
+          categories: model.company.categories,
+          violations: model.company.violations.map(item => item.name)
+        },
+      };
+      return response.send(newModel);
     }
   });
 };
