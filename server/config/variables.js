64da3590d1eaf3361d8918b77b7ede2e73734e5d
ny1am <ny1am.lviv.ua@gmail.com>
2018-02-14 20:19:04 +0200

    fix: prod settings

---
+++ server/config/variables.js
@@ -0,0 +1,48 @@
+module.exports = {
+  baseUrl: 'https://www.itakpoimut.info',
+
+  sessionSecret: process.env.EXPRESS_SESSION_SECRET,
+
+  cloudinary: {
+    cloud_name: process.env.cloudinary_cloud_name,
+    api_key: process.env.cloudinary_api_key,
+    api_secret: process.env.cloudinary_api_secret
+  },
+
+  elasticsearch: {
+    host: process.env.elasticsearch_host,
+    apiVersion: '2.4'
+  },
+
+  mongoUrl: process.env.MONGODB_URL,
+
+  rollbar: process.env.ROLLBAR,
+
+  sparkpost: {
+    key: process.env.sparkpost_key,
+    email: 'itakpoimut@' + process.env.SPARKPOST_SANDBOX_DOMAIN
+  },
+
+  facebook: {
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+  },
+
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  },
+
+  dropbox: {
+    app: {
+      app_key: process.env.DROPBOX_APP_KEY, 
+      app_secret: process.env.DROPBOX_APP_SECRET
+    },
+    access_token: {
+      oauth_token_secret: process.env.DROPBOX_TOKEN_SECRET,
+      oauth_token: process.env.DROPBOX_TOKEN,
+      uid: process.env.DROPBOX_TOKEN_UID
+    }
+  }
+
+};
