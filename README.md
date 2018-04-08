c0066bdf32605c5a8590f4603b2a786d903baf18
Nazar Yavnyy <nazar.yavnyy@gmail.com>
2018-04-08 16:30:50 +0300

    refa: organize env variables

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Project requires npm (min v 3.10) and node (min v 7.0.0)
 
-For dev purposes run command:
-`npm start`
-then open http://localhost:5000/ in your browser
+For dev purposes run commands:
+1. `npm install`
+2. `npm run start-dev`
+then open `http://localhost:5001/companies` in your browser to make sure it works
--- server/config/variables.js
+++ server/config/variables.js
@@ -1,48 +1,48 @@
 module.exports = {
-  baseUrl: 'https://www.itakpoimut.info',
+	baseUrl: process.env.BASE_URL,
 
-  sessionSecret: process.env.EXPRESS_SESSION_SECRET,
+	sessionSecret: process.env.EXPRESS_SESSION_SECRET,
 
-  cloudinary: {
-    cloud_name: process.env.cloudinary_cloud_name,
+	cloudinary: {
+		cloud_name: process.env.cloudinary_cloud_name,
     api_key: process.env.cloudinary_api_key,
     api_secret: process.env.cloudinary_api_secret
-  },
+	},
 
-  elasticsearch: {
-    host: process.env.elasticsearch_host,
-    apiVersion: '2.4'
-  },
+	elasticsearch: {
+		host: process.env.elasticsearch_host,
+		apiVersion: '2.4'
+	},
 
-  mongoUrl: process.env.MONGODB_URL,
+	mongoUrl: process.env.MONGODB_URL,
 
-  rollbar: process.env.ROLLBAR,
+	rollbar: process.env.ROLLBAR,
 
-  sparkpost: {
-    key: process.env.sparkpost_key,
-    email: 'itakpoimut@' + process.env.SPARKPOST_SANDBOX_DOMAIN
-  },
+	sparkpost: {
+		key: process.env.sparkpost_key,
+		email: process.env.SPARKPOST_EMAIL_FROM
+	},
 
-  facebook: {
-    clientID: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-  },
+	facebook: {
+		clientID: process.env.FACEBOOK_CLIENT_ID,
+		clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+	},
 
-  google: {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  },
+	google: {
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET
+	},
 
-  dropbox: {
-    app: {
-      app_key: process.env.DROPBOX_APP_KEY, 
-      app_secret: process.env.DROPBOX_APP_SECRET
-    },
-    access_token: {
-      oauth_token_secret: process.env.DROPBOX_TOKEN_SECRET,
-      oauth_token: process.env.DROPBOX_TOKEN,
-      uid: process.env.DROPBOX_TOKEN_UID
-    }
-  }
+	dropbox: {
+		app: {
+			app_key: process.env.DROPBOX_APP_KEY, 
+			app_secret: process.env.DROPBOX_APP_SECRET
+		},
+		access_token: {
+			oauth_token_secret: process.env.DROPBOX_TOKEN_SECRET,
+			oauth_token: process.env.DROPBOX_TOKEN,
+			uid: process.env.DROPBOX_TOKEN_UID
+		}
+	}
 
 };
