57e00edee39b7b65a329054012ed011d7c62f87f
Nazar Yavnyy <nazar.yavnyy@gmail.com>
2018-04-20 11:20:09 +0300

    return comment with user info on comment add

--- server/api/addComment.js
+++ server/api/addComment.js
@@ -1,20 +1,28 @@
-var Comment = require('mongoose').model('Comment');
+var Comment = require("mongoose").model("Comment");
 
 exports.post = function(params, callback) {
-	var commentData = {
-		_user: params.userId,
-		_company: params.companyId,
-		text: params.commentText,
-		created: Date.now()
-	};
-	Comment.create(commentData, function(err, doc) {
-		if (err) {
-			return callback(err);
-		} else {
-			return callback(null, {
-				result: 'success',
-				comment: doc
-			});
-		}
-	});
+  var commentData = {
+    _user: params.userId,
+    _company: params.companyId,
+    text: params.commentText,
+    created: Date.now()
+  };
+  Comment.create(commentData, function(err, doc) {
+    if (err) {
+      return callback(err);
+    } else {
+      Comment.findOne({ _id: doc._id })
+        .populate("_user", "fname lname picture_url")
+        .exec(function(err, doc) {
+          if (err) {
+            return callback(err);
+          } else {
+            return callback(null, {
+              result: "success",
+              comment: doc
+            });
+          }
+        });
+    }
+  });
 };
--- server/api/comments.js
+++ server/api/comments.js
@@ -1,33 +1,37 @@
-var Company = require('mongoose').model('Company');
-var Comment = require('mongoose').model('Comment');
-var asyncjs = require('async');
+var Company = require("mongoose").model("Company");
+var Comment = require("mongoose").model("Comment");
+var asyncjs = require("async");
 
 var recordsPerPage = 5;
 
 exports.get = function(params, callback) {
   var currentPage = params.currentPage || 1;
   var companyId = params.companyId;
-  asyncjs.parallel({
-    comments: function(next) {
-      Comment
-      .find({_company: companyId})
-      .skip((currentPage - 1) * recordsPerPage)
-      .limit(recordsPerPage)
-      .sort({_id: 'desc'})
-            .populate('_user', 'fname lname picture_url')
-      .exec(next);
+  asyncjs.parallel(
+    {
+      comments: function(next) {
+        Comment.find({ _company: companyId })
+          .skip((currentPage - 1) * recordsPerPage)
+          .limit(recordsPerPage)
+          .sort({ _id: "desc" })
+          .populate("_user", "fname lname picture_url")
+          .exec(next);
+      },
+      commentsCount: function(next) {
+        Comment.count({ _company: companyId }).exec(next);
+      }
     },
-    commentsCount: function(next) {
-      Comment.count({_company: companyId}).exec(next);
+    function(err, model) {
+      if (err) {
+        return callback(err);
+      } else {
+        model.currentPage = currentPage;
+        model.recordsPerPage = recordsPerPage;
+        model.totalPages = Math.ceil(
+          model.commentsCount / model.recordsPerPage
+        );
+        return callback(null, model);
+      }
     }
-  }, function(err, model) {
-    if(err) {
-      return callback(err);
-    } else {
-      model.currentPage = currentPage;
-      model.recordsPerPage = recordsPerPage;
-      model.totalPages = Math.ceil(model.commentsCount / model.recordsPerPage);
-      return callback(null, model);
-    }
-  });
+  );
 };
